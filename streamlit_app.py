a5d9f3eb502958b47dfa64c561d9d8cb10aea943
Muhammad Arif Azhar bin Zaihan <gf240032@student.uthm.edu.my>
2025-08-13 16:23:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.set_page_config(page_title="SVR Rainfall Landslide Prediction", layout="wide"
 
 # Title and description
 st.title("📈 Support Vector Regression to Predict Slope Stability Considering Rainfall and Soil Strength Effect")
-st.info("This app builds and trains an SVR model to predict Factor of Safety from rainfall and soil strength parameters.")
+st.info("This app builds and trains an SVR model to predict Factor of Safety from rainfall and soil strength parameters. Created by Arif Azhar.")
 
 # Step 1: Load Data
 with st.expander('Data'):
